699609769f3bd6cc47099c9008f9cb572ecb1173
Benjamin Henderson <b.hen02@icloud.com>
2022-11-24 13:01:39 +0000

    Found lines added per author

--- preprocess.py
+++ preprocess.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 # Data frame containing the first dataset as df:
 df = pd.read_json('firstdataset.json')
@@ -14,3 +15,16 @@ year = df['year']
 # Lines - will show lines added and lines deleted respectively
 lines_added = df['addedloc']  # Assuming that the headers mean adding and deleting lines
 lines_deleted = df['deletedloc']
+
+# Array of all the authors that have commited
+authors_unique = np.unique(np.array(author))
+
+# Finds the lines commited per author
+lines_per_author = np.zeros(shape=authors_unique.shape)
+i = 0
+for a in author:
+    for auth in authors_unique:
+        if a == auth:
+            idx = np.where(authors_unique==auth)
+            lines_per_author[idx] += lines_added[i]
+    i += 1
