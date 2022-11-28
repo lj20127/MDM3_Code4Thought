b0a5f652e980d2be37a167fdb2bce1633d5da646
Benjamin Henderson <b.hen02@icloud.com>
2022-11-28 15:48:37 +0000

    Making the java filtering more efficient

--- preprocess.py
+++ preprocess.py
@@ -4,26 +4,21 @@ import numpy as np
 # Data frame containing the first dataset as df:
 df = pd.read_json('firstdataset.json')
 
-# REMOVING ALL TECHNOLOGIES APART FROM JAVA:
-java_df = []
-java_index = []
-for i in range(len(df)):
-    if df.iloc[i]['technology'] == 'java' and 'Rename' not in df.iloc[i]['subject']:
-        java_index.append(i)
-        java_df.append(df.iloc[i])
+# removing all technologies apart from java
+java_idx = df[df['technology'] == 'java'].index
+df.drop(index=df[df['technology'] != 'java'].index, axis=0, inplace=True)
 
 # Splitting the data into relevant variables:
-author = java_df['author']  # simply shows all corresponding authors to each commit
+author = df['author']  # simply shows all corresponding authors to each commit
 
 # DATES - day/month/year
-day = java_df['day']
-month = java_df['month']
-year = java_df['year']
+day = df['day']
+month = df['month']
+year = df['year']
 
 # Lines - will show lines added and lines deleted respectively
-lines_added = java_df['addedloc']  # Assuming that the headers mean adding and deleting lines
-lines_deleted = java_df['deletedloc']
-
+lines_added = df['addedloc']  # Assuming that the headers mean adding and deleting lines
+lines_deleted = df['deletedloc']
 
 # Array of all the authors that have commited
 authors_unique = np.unique(np.array(author))
@@ -36,7 +31,5 @@ for a in author:
     for auth in authors_unique:
         if a == auth:
             idx = np.where(authors_unique==auth)
-            lines_per_author[idx] += lines_added[i]
+            lines_per_author[idx] += lines_added[java_idx[i]]
     i += 1
-
-
