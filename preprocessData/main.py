0da7c1a9c8a24d07d9b705d2447db123e9285e04
Benjamin Henderson <b.hen02@icloud.com>
2022-12-02 16:33:11 +0000

    Creating average lines per author plot and moving merging_lists.py

--- preprocessData/main.py
+++ preprocessData/main.py
@@ -1,16 +1,34 @@
 from extractJson import extractJson
 from lpa import lpa
+import os
+import numpy as np
+from merging_lists import merge_lists
+import matplotlib.pyplot as plt
 
 # Specify folder path that contains the json files
-path = r""
+path = os.path.dirname(os.path.realpath(__file__))+"/systems/"
 
 # List containing all the json files as panda dataframes
-all_dfs = extractJson(path, 2)
-# extracting first df
-first_df = all_dfs[0]
-# extracting only java entries
-java_df = first_df[first_df.technology == "java"]
+all_dfs = extractJson(path)
 
-# create new dataframe with 'lpa' column
-new_df = lpa(java_df)
-print(new_df)
+# creates a list for lpa for every json file
+lists = [[] for i in range(0,len(all_dfs))]
+i=0
+threshold = 1000
+for df in all_dfs:
+    java_df = df[df.technology == "java"]
+    new_df = lpa(java_df)
+    # removes any lpa over threshold
+    new_df.loc[new_df.lpa > threshold] = 0
+    if len(np.array(new_df["lpa"])) > 0:
+        lists[i] = np.array(new_df["lpa"])
+    i+=1
+
+# removes any lpa lists that are empty
+lists = [lst for lst in lists if lst != []]
+
+# creats lpa vs weeks plot
+output = merge_lists(lists)
+weeks = np.linspace(0,output[1],output[1])
+plt.plot(weeks,output[0])
+plt.show()
--- preprocessData/merging_lists.py
+++ preprocessData/merging_lists.py
@@ -1,7 +1,7 @@
 from statistics import mean
 import numpy as np
 
-## Merges lists of different lenghts. Finds 'time series'
+## Merges lists of different lenghts. Finds average 'time series'
 def merge_lists(lists): 
     maxlen = max([len(lst) for lst in lists])
     model = np.linspace(0,1,maxlen)
@@ -11,10 +11,10 @@ def merge_lists(lists):
         interped_lists[i] = np.interp(model,np.linspace(0, 1, len(lst)),lst)
         i+=1
     merged = [mean(i) for i in zip(*interped_lists)]
-    return merged
+    return merged,maxlen
 
 ## To test the function
 if __name__ == "__main__":
-    lists = [[1,2,3,4,5,7,8], [5,2,3], [1,3,5,4,6]]
-    merged = merge_lists(lists)
-    print(merged)
+    lists = [[1,2,3,4,5], [1,3,5]]
+    output = merge_lists(lists)
+    print(output[0])
