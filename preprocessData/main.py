32c486b1f406650d1256008962bf5b1ee793a0e6
Benjamin Henderson <b.hen02@icloud.com>
2022-12-15 16:31:15 +0000

    Added confidence interval bands to plot

--- preprocessData/main.py
+++ preprocessData/main.py
@@ -10,7 +10,7 @@ from change_pts import detect_change_pts
 path = os.path.dirname(os.path.realpath(__file__))+"/systems/"
 
 # List containing all the json files as panda dataframes
-all_dfs = extractJson(path,10) # use 'all_dfs = extractJson(path,n)' for n random json files 
+all_dfs = extractJson(path) # use 'all_dfs = extractJson(path,n)' for n random json files 
 
 # creates a list for lpa for every json file
 lists = [[] for i in range(0,len(all_dfs))]
@@ -33,9 +33,11 @@ lists = [lst for lst in lists if lst != []]
 # creates lpa vs weeks plot
 output = merge_lists(lists)
 merged=output[0]
-num_weeks=output[1]
+ci=output[1]
+num_weeks=output[2]
 weeks = np.linspace(0,num_weeks,num_weeks)
 plt.plot(weeks,merged)
+plt.fill_between(weeks,(np.array(merged)-np.array(ci)),(np.array(merged)+np.array(ci)),color='red',alpha=0.3)
 plt.show()
 
 # finds 2 change points
--- preprocessData/merging_lists.py
+++ preprocessData/merging_lists.py
@@ -1,5 +1,7 @@
 from statistics import mean
+from math import sqrt
 import numpy as np
+import matplotlib.pyplot as plt
 
 ## Merges lists of different lenghts. Finds average 'time series'
 def merge_lists(lists): 
@@ -10,11 +12,18 @@ def merge_lists(lists):
     for lst in lists:
         interped_lists[i] = np.interp(model,np.linspace(0, 1, len(lst)),lst)
         i+=1
+
+    # finds confidence interval of each week
+    confidence_interval = [0.95 * np.std(i) / sqrt(len(i)) for i in zip(*interped_lists)]
+    # finds mean of each week
     merged = [mean(i) for i in zip(*interped_lists)]
-    return merged,maxlen
+    return merged,confidence_interval,maxlen
 
 ## To test the function
 if __name__ == "__main__":
-    lists = [[1,2,3,4,5], [1,3,5]]
+    lists = [[1,5,4,2,2,3,4,5], [5,6,4,2,1,3,5]]
     output = merge_lists(lists)
-    print(output[0])
+    print(output)
+    plt.plot(output[0])
+    plt.fill_between(np.linspace(0,output[2]-1,output[2]),(np.array(output[0])-np.array(output[1])), (np.array(output[0])+np.array(output[1])), color='red', alpha=0.3)
+    plt.show()
