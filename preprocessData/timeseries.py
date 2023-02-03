3d1d98637160cd9c453fae9dab7c8de158834101
Benjamin Henderson <b.hen02@icloud.com>
2023-02-03 16:55:00 +0000

    Fixed Dickey fuller test and added kpss test for stationarity. Also added ways of finding p,d,q values fro ARIMA model

--- preprocessData/timeseries.py
+++ preprocessData/timeseries.py
@@ -4,9 +4,11 @@
 
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import pandas as pd 
 import matplotlib.pyplot as plt  
 from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.stattools import kpss
 
 
 
@@ -25,14 +27,31 @@ def DickeyFuller(dfseries):
     print('ADF critical values:', crit_vals)
     print('ADF best information criterion:', icbest)
     
-    if pval < crit_vals[0]:
-        print('H0 rejected with a significance level of less than 1%')
-    elif pval < crit_vals[1] and pval > crit_vals[0]:
-        print('H0 rejected with a significance level of less than 5%')
-    elif pval < crit_vals[2] and pval > crit_vals[1]:
-        print('H0 rejected with a significance level of less than 10%')
+    if pval < 0.05:
+        if adf < crit_vals['1%']:
+            print('H0 rejected with a significance level of less than 1%')
+            print('i.e. Time series is stationary')
+        elif adf < crit_vals['5%'] and adf > crit_vals['1%']:
+            print('H0 rejected with a significance level of less than 5%')
+            print('i.e. Time series is stationary')
+        elif adf < crit_vals['10%'] and adf > crit_vals['5%']:
+            print('H0 rejected with a significance level of less than 10%')
+            print('i.e. Time series is stationary')
+        else:
+            pass
     else:
         print('Time series is non-stationary')
+
+
+def kpss_test(dfseries,**kw):
+    statistic, p_value, n_lags, critical_values = kpss(dfseries, **kw)
+    print(f'KPSS Statistic: {statistic}')
+    print(f'p-value: {p_value}')
+    print(f'num lags: {n_lags}')
+    print('Critical Values:')
+    for key, value in critical_values.items():
+        print(f'    {key} : {value}')
+    print(f'Result: The series is {"not " if p_value < 0.05 else ""}stationary')
 
 
 def SeasonalityCheck(dfseries):
@@ -46,23 +65,33 @@ def SeasonalityCheck(dfseries):
 #ARIMA MODEL
 
 def FitARIMA(dfseries,plot=False):
-    model = ARIMA(dfseries, order=(0,2,12))
+    # should both return stationary and proves that d=1
+    kpss_test(dfseries.diff().dropna())
+    DickeyFuller(dfseries.diff().dropna())
+
+    model = ARIMA(dfseries, order=(1,1,3))
     modelfit = model.fit()
-    print(modelfit.summary())  
-    
+    # print(modelfit.summary())  
+
     # Plotting Residual Errors  
     #There is a bias in the prediction (a non-zero mean in the residuals)
     myresiduals = pd.DataFrame(modelfit.resid) 
     if plot:
+        # shows what p and q values we should use for ARIMA model
+        fig, (ax1, ax2) = plt.subplots(2)
+        plot_acf(dfseries.diff().dropna(), ax=ax1)
+        plot_pacf(dfseries.diff().dropna(), ax=ax2)
+        plt.show()
+        
         fig, ax = plt.subplots(1,2)  
         myresiduals.plot(title = "Residuals", ax = ax[0])  
         myresiduals.plot(kind = 'kde', title = 'Density', ax = ax[1])  
         plt.show()  
     
-    # Actual vs Fitted  
+    # Actual vs Fitted
     model_values = modelfit.predict()
     
-    return model_values
+    return model_values,myresiduals
     
     
     
