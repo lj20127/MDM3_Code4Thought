c3323d6b10a4599aa1984f92bd359b5ab16b3584
Benjamin Henderson <b.hen02@icloud.com>
2023-02-08 18:40:14 +0000

    Changed model. Also, added function to find different number of change points

--- preprocessData/change_pts.py
+++ preprocessData/change_pts.py
@@ -2,9 +2,19 @@ import numpy as np
 import ruptures as rpt
 import matplotlib.pyplot as plt
 
+def unknown_n_bkps(rpt_model): # finds number of change points for different penalty values
+    num_change_pts = []
+    for i in range(0,1000):
+        change_pts = rpt_model.predict(pen=i)
+        num_change_pts.append(len(change_pts)-1)
+    plt.hist(num_change_pts,bins=[0,1,2,3,4])
+    plt.title("Distsribution of change points for different penalties")
+    plt.show()
+
 def detect_change_pts(model,n):
-    rpt_list = rpt.Binseg(model="l2").fit(model)
-    change_pts = rpt_list.predict(n_bkps=n)
+    rpt_model = rpt.Binseg(model="rbf").fit(model)
+    unkown_n_bkps(rpt_model)
+    change_pts = rpt_model.predict(n_bkps=n)
     return change_pts
 
 if __name__ == "__main__":
