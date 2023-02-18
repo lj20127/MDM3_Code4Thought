26085bfc2050227681a9c9ea5851a5c3928648f1
qcuincy <quincy.sproul@gmail.com>
2023-02-18 17:24:54 +0000

    reorginse files

--- model_gui/main.py
+++ model_gui/main.py
@@ -4,8 +4,7 @@ from dtools.dataprocess import DataProcessor
 from dash.exceptions import PreventUpdate
 from dtools.functions import *
 from dtools.walks import *
-from dash import dcc
-from dash import html
+from dash import dcc, html, dash_table
 import plotly.graph_objects as go
 import plotly.express as px
 import plotly.express as px
@@ -40,7 +39,7 @@ app.layout = html.Div([
         min=0,
         max=10,
         step=1,
-        value=4,
+        value=3,
         marks={
             0: {'label': '0'},
             5: {'label': '5'},
@@ -62,7 +61,7 @@ app.layout = html.Div([
         min=0,
         max=10,
         step=1,
-        value=4,
+        value=3,
         marks={
             0: {'label': '0'},
             5: {'label': '5'},
@@ -71,27 +70,27 @@ app.layout = html.Div([
         tooltip = { 'always_visible': True }
     ),
     html.Hr(),
-    # html.H3("Upload Test Data"),
-    # dcc.Upload(
-    #     id='upload-data',
-    #     children=html.Div([
-    #         'Drag and Drop or ',
-    #         html.A('Select Files')
-    #     ]),
-    #     style={
-    #         'width': '50%',
-    #         'height': '60px',
-    #         'lineHeight': '60px',
-    #         'borderWidth': '1px',
-    #         'borderStyle': 'dashed',
-    #         'borderRadius': '5px',
-    #         'textAlign': 'center',
-    #         'margin': '10px'
-    #     },
-    #     multiple=False
-    # ),
-    # html.Div(id='output-data-upload'),
-    # html.Button('Calculate MAPE', id='calc-mape', n_clicks=0),
+    html.H3("Upload Test Data"),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '50%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+    html.Div(id='processed-data-upload'),
     html.Hr(),
     html.Div([
         dcc.Graph(id='short-term-plot', style={'display': 'inline-block', 'width': '49%'}),
@@ -99,129 +98,240 @@ app.layout = html.Div([
     ])
 ])
 
-# # Define the callback to calculate the MAPE
-# @app.callback(Output('calc-mape', 'children'),
-#               Input('upload-data', 'contents'),
-#               State('upload-data', 'filename'),
-#               State('short-term-model', 'value'),
-#               State('num-change-points', 'value'))
-# def calculate_mape(contents, filename, short_model_type, num_change_points):
-#     if contents is not None:
-#         # df = pd.read_json(contents, orient='split')
-#         # Get the predictions
-#         if short_model_type == 'arima':
-#             model = ARIMA(df, order=(1, 1, 0))
-#             predictions = model.fit().predict()
-#         else:
-#             predictions = np.cumsum(np.random.randn(len(df)))
-#         # Calculate the MAPE
-#         test_data = df[num_change_points:]
-#         pred_data = predictions[num_change_points:]
-#         mape = np.mean(np.abs((test_data - pred_data) / test_data)) * 100
-#         return html.Div([
-#         html.H4("Mean Absolute Percentage Error: {:.2f}%".format(mape))
-#         ])
-#     else:
-#         return html.Div('Please upload a file.')
-    
-# import json
-# @app.callback(
-#     Output("output-data-upload", "children"),
-#     Input("upload-data", "contents"),
-#     State("upload-data", "filename"),
-# )
-# def on_data_upload(contents, filename):
-#     if contents is None:
-#         raise PreventUpdate
-
-#     if not filename.endswith(".json"):
-#         return "Please upload a file with the .json extension"
-#     # content_type, content_string = contents[0].split(',')
-#     jsonData = json.loads(contents)
-#     print(jsonData)
-#     decoded = base64.b64decode(contents)
-#     df = pd.read_csv(io.StringIO(decoded.decode('utf-8')))
-#     # content_type, content_string = contents.split(",")
-#     # decoded = base64.b64decode(content_string)
-    
-#     # # this is a dict!
-#     # content_dict = json.loads(decoded)
-    
-#     # df = pd.read_json(contents)
-#     lpa_df = lines_per_author(df)
-
-#     return dcc.Graph(figure=lpa_df)
-
-
+              
 @app.callback(Output('short-term-plot', 'figure'),
                 Input('short-term-model', 'value'),
-                Input('num-change-points', 'value'))
+                Input('num-change-points', 'value'),
+                Input('upload-data', 'contents'),
+                State('upload-data', 'filename'))
                 # State('upload-data', 'contents'))
-def update_short_term_plot(short_model_type, num_change_points):
+def update_short_term_plot(short_model_type, num_change_points, list_of_contents, list_of_names):
+    if list_of_contents is not None:
+                # Parse the contents of the file
+        children = [parse_contents(c, n) for c,n in zip(list_of_contents, list_of_names)]
 
-    if short_model_type == 'arima':
-        data = full_arima_model["short_model"]
-        data["model"] = data["model"][1:]
-        data["time"] = data["time"][1:]
+        processed_data = process_data(children[-1])
+
+        if short_model_type == 'arima':
+            data = full_arima_model["short_model"]
+            data["model"] = data["model"][1:]
+            data["time"] = data["time"][1:]
+        else:
+            data = full_rw_model["short_model"]
+        # Create the plot
+        HSV_tuples = [(x*1.0/num_change_points, 0.5, 0.5) for x in range(num_change_points)]
+        RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
+        df = pd.DataFrame(dict(time=data["time"], model=data["model"]))
+
+        chng_dfs, chng_xs = get_change_dfs(df, data["model"], num_change_points)
+
+        processed_dfs = [processed_data[(chng_xs[i][0] <= processed_data["week"]) & (processed_data["week"] <= chng_xs[i][-1])] for i in range(len(chng_xs))]
+        processed_dfs_avgs = [df.week_linesperauthor.mean() for df in processed_dfs]
+
+        chng_dfs_avgs = [df.model.mean() for df in chng_dfs]
+
+        fig = go.Figure()
+        for i in range(num_change_points):
+            col = "rgba(" + ", ".join([str(l) for l in RGB_tuples[i]]) + ", 0.2)"
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, fillcolor=col, fill='tozeroy',mode='none', name=f"")) # override default markers+lines))
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, mode='lines',opacity=1,line=dict(color=col, width=1.5), name=""))
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=[chng_dfs_avgs[i] for _ in range(len(chng_dfs[i].time))], name=f"Phase {i+1} Model Avg"))
+            fig.add_trace(go.Scatter(x=processed_dfs[i].week, y=[processed_dfs_avgs[i] for _ in range(len(processed_dfs[i].week))], name=f"Phase {i+1} Test Data Avg"))
+            
+            fig.add_trace(go.Scatter(x=[chng_dfs[i].time.median() for _ in range(len(chng_dfs[i].time))], y=[processed_dfs_avgs[i] for _ in range(int(len(processed_dfs[i].week)/2))] + [chng_dfs_avgs[i] for _ in range(int(len(chng_dfs[i].time)/2))], fill="toself", name=f""))
+            
+            xloc = chng_dfs[i].time.median()
+            smaller = min([processed_dfs_avgs[i], chng_dfs_avgs[i]])
+            bigger = max([processed_dfs_avgs[i], chng_dfs_avgs[i]])
+            yloc = smaller + (bigger - smaller)/2
+            plus_minus = "+" if (chng_dfs_avgs[i] == smaller) else "-"
+
+            increase = (bigger-smaller)/smaller
+            decrease = (bigger-smaller)/bigger
+            diff = increase if (chng_dfs_avgs[i] == smaller) else decrease
+            text = "PCT Change" + plus_minus + str(round(diff*100, 2)) + "%"
+            fig.add_annotation(
+                x=xloc, y=yloc,
+                text=text,
+                showarrow=False
+            )
+        fig.update_layout(
+            title='Short-term Model: {}'.format(short_model_type),
+            xaxis_title='Weeks',
+            yaxis_title='Lines Per Author',
+        )
+        for trace in fig["data"]:
+            if trace["name"].split(" ")[0] != "Phase": trace["showlegend"] = False
+        return fig
     else:
-        data = full_rw_model["short_model"]
-    # Create the plot
-    HSV_tuples = [(x*1.0/num_change_points, 0.5, 0.5) for x in range(num_change_points)]
-    RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
-    df = pd.DataFrame(dict(time=data["time"], model=data["model"]))
+        if short_model_type == 'arima':
+            data = full_arima_model["short_model"]
+            data["model"] = data["model"][1:]
+            data["time"] = data["time"][1:]
+        else:
+            data = full_rw_model["short_model"]
+        # Create the plot
+        HSV_tuples = [(x*1.0/num_change_points, 0.5, 0.5) for x in range(num_change_points)]
+        RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
+        df = pd.DataFrame(dict(time=data["time"], model=data["model"]))
 
-    chng_dfs = get_change_dfs(df, data["model"], num_change_points)
+        chng_dfs, chng_xs = get_change_dfs(df, data["model"], num_change_points)
 
-    fig = go.Figure()
-    for i in range(num_change_points):
-        col = "rgba(" + ", ".join([str(l) for l in RGB_tuples[i]]) + ", 0.8)"
-        fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, fillcolor=col, fill='tozeroy',mode='none', name=f"Phase {i+1}")) # override default markers+lines))
-        fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, mode='lines',opacity=1,line=dict(color=col, width=1.5), name=""))
+        fig = go.Figure()
+        for i in range(num_change_points):
+            col = "rgba(" + ", ".join([str(l) for l in RGB_tuples[i]]) + ", 0.8)"
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, fillcolor=col, fill='tozeroy',mode='none', name=f"Phase {i+1}")) # override default markers+lines))
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, mode='lines',opacity=1,line=dict(color=col, width=1.5), name=""))
 
 
-    fig.update_layout(
-        title='Short-term Model: {}'.format(short_model_type),
-        xaxis_title='Weeks',
-        yaxis_title='Lines Per Author',
-    )
-    for trace in fig["data"]:
-        if trace["name"].split(" ")[0] != "Phase": trace["showlegend"] = False
-    return fig
+        fig.update_layout(
+            title='Short-term Model: {}'.format(short_model_type),
+            xaxis_title='Weeks',
+            yaxis_title='Lines Per Author',
+        )
+        for trace in fig["data"]:
+            if trace["name"].split(" ")[0] != "Phase": trace["showlegend"] = False
+        return fig
     
+
 
 @app.callback(Output('long-term-plot', 'figure'),
                 Input('long-term-model', 'value'),
-                Input('num-change-points-long', 'value'))
-                # State('upload-data', 'contents'))
-def update_long_term_plot(long_model_type, num_change_points_long):
-    if long_model_type == 'arima':
-        data = full_arima_model["long_model"]
-        data["model"] = data["model"][1:]
-        data["time"] = data["time"][1:]
+                Input('num-change-points-long', 'value'),
+                Input('upload-data', 'contents'),
+                State('upload-data', 'filename'))
+def update_long_term_plot(long_model_type, num_change_points_long, list_of_contents, list_of_names):
+    if list_of_contents is not None:
+        children = [parse_contents(c, n) for c,n in zip(list_of_contents, list_of_names)]
+
+        # # Process the data
+        processed_data = process_data(children[-1])
+        if long_model_type == 'arima':
+            data = full_arima_model["long_model"]
+            data["model"] = data["model"][1:]
+            data["time"] = data["time"][1:]
+        else:
+            data = full_rw_model["long_model"]
+        # Create the plot
+        HSV_tuples = [(x*1.0/num_change_points_long, 0.5, 0.5) for x in range(num_change_points_long)]
+        RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
+        df = pd.DataFrame(dict(time=data["time"], model=data["model"]))
+
+        chng_dfs, chng_xs = get_change_dfs(df, data["model"], num_change_points_long)
+
+        processed_dfs = [processed_data[(chng_xs[i][0] <= processed_data["week"]) & (processed_data["week"] <= chng_xs[i][-1])] for i in range(len(chng_xs))]
+        processed_dfs_avgs = [df.week_linesperauthor.mean() for df in processed_dfs]
+
+        chng_dfs_avgs = [df.model.mean() for df in chng_dfs]
+
+        fig = go.Figure()
+        for i in range(num_change_points_long):
+            col = "rgba(" + ", ".join([str(l) for l in RGB_tuples[i]]) + ", 0.2)"
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, fillcolor=col, fill='tozeroy',mode='none', name=f"")) # override default markers+lines))
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, mode='lines',opacity=1,line=dict(color=col, width=1.5), name=""))
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=[chng_dfs_avgs[i] for _ in range(len(chng_dfs[i].time))], name=f"Phase {i+1} Model Avg"))
+            fig.add_trace(go.Scatter(x=processed_dfs[i].week, y=[processed_dfs_avgs[i] for _ in range(len(processed_dfs[i].week))], name=f"Phase {i+1} Test Data Avg"))
+            
+            fig.add_trace(go.Scatter(x=[chng_dfs[i].time.median() for _ in range(len(chng_dfs[i].time))], y=[processed_dfs_avgs[i] for _ in range(int(len(processed_dfs[i].week)/2))] + [chng_dfs_avgs[i] for _ in range(int(len(chng_dfs[i].time)/2))], fill="toself", name=f""))
+            
+            xloc = chng_dfs[i].time.median()
+            smaller = min([processed_dfs_avgs[i], chng_dfs_avgs[i]])
+            bigger = max([processed_dfs_avgs[i], chng_dfs_avgs[i]])
+            yloc = smaller + (bigger - smaller)/2
+            plus_minus = "+" if (chng_dfs_avgs[i] == smaller) else "-"
+
+            increase = (bigger-smaller)/smaller
+            decrease = (bigger-smaller)/bigger
+            diff = increase if (chng_dfs_avgs[i] == smaller) else decrease
+            text = "PCT Change" + plus_minus + str(round(diff*100, 2)) + "%"
+            fig.add_annotation(
+                x=xloc, y=yloc,
+                text=text,
+                showarrow=False
+            )
+        fig.update_layout(
+            title='Long-term Model: {}'.format(long_model_type),
+            xaxis_title='Weeks',
+            yaxis_title='Lines Per Author',
+        )
+
+        for trace in fig["data"]:
+            if trace["name"].split(" ")[0] != "Phase": trace["showlegend"] = False
+        return fig
     else:
-        data = full_rw_model["long_model"]
-    # Create the plot
-    HSV_tuples = [(x*1.0/num_change_points_long, 0.5, 0.5) for x in range(num_change_points_long)]
-    RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
-    df = pd.DataFrame(dict(time=data["time"], model=data["model"]))
+        if long_model_type == 'arima':
+            data = full_arima_model["long_model"]
+            data["model"] = data["model"][1:]
+            data["time"] = data["time"][1:]
+        else:
+            data = full_rw_model["long_model"]
+        # Create the plot
+        HSV_tuples = [(x*1.0/num_change_points_long, 0.5, 0.5) for x in range(num_change_points_long)]
+        RGB_tuples = list(map(lambda x: colorsys.hsv_to_rgb(*x), HSV_tuples))
+        df = pd.DataFrame(dict(time=data["time"], model=data["model"]))
 
-    chng_dfs = get_change_dfs(df, data["model"], num_change_points_long)
+        chng_dfs, chng_xs = get_change_dfs(df, data["model"], num_change_points_long)
 
-    fig = go.Figure()
-    for i in range(num_change_points_long):
-        col = "rgba(" + ", ".join([str(l) for l in RGB_tuples[i]]) + ", 0.8)"
-        fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, fillcolor=col, fill='tozeroy',mode='none', name=f"Phase {i+1}")) # override default markers+lines))
-        fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, mode='lines',opacity=1,line=dict(color=col, width=1.5), name=""))
+        fig = go.Figure()
+        for i in range(num_change_points_long):
+            col = "rgba(" + ", ".join([str(l) for l in RGB_tuples[i]]) + ", 0.8)"
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, fillcolor=col, fill='tozeroy',mode='none', name=f"Phase {i+1}")) # override default markers+lines))
+            fig.add_trace(go.Scatter(x=chng_dfs[i].time, y=chng_dfs[i].model, mode='lines',opacity=1,line=dict(color=col, width=1.5), name=""))
 
-    fig.update_layout(
-        title='Long-term Model: {}'.format(long_model_type),
-        xaxis_title='Weeks',
-        yaxis_title='Lines Per Author',
-    )
+        fig.update_layout(
+            title='Long-term Model: {}'.format(long_model_type),
+            xaxis_title='Weeks',
+            yaxis_title='Lines Per Author',
+        )
 
-    for trace in fig["data"]:
-        if trace["name"].split(" ")[0] != "Phase": trace["showlegend"] = False
-    return fig
+        for trace in fig["data"]:
+            if trace["name"].split(" ")[0] != "Phase": trace["showlegend"] = False
+        return fig
+
+
+def parse_contents(contents, filename):
+
+    content_type, content_string = contents.split(",")
+
+    decoded = base64.b64decode(content_string)
+    
+    try:
+        decoded = io.StringIO(decoded.decode('utf-8'))
+        df = pd.read_json(decoded)
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    # Add a column with the filename
+    df['filename'] = filename
+
+    return df
+
+def process_data(df):
+    clean_data = clean_dataframe(df)
+    processed_data = filter_dataframe(clean_data, "java")
+    lpa_data = lines_per_author(processed_data, timeframe="week")
+
+    return lpa_data
+
+@app.callback(Output('output-data-upload', 'children'),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'))
+def update_output(list_of_contents, list_of_names):
+    
+    if list_of_contents is not None:
+        # Parse the contents of the file
+        children = [parse_contents(c, n) for c,n in zip(list_of_contents, list_of_names)]
+
+        # # Process the data
+        processed_data = process_data(children[-1])
+
+        return f"Loaded {list_of_names[-1]} successfully!"
+
+
+    return html.Div(['No data has been uploaded yet.'])
     
 
 def get_change_dfs(df, model, n):
@@ -229,6 +339,8 @@ def get_change_dfs(df, model, n):
     change_pts.insert(0, 0)
     xs = [[j for j in range(change_pts[i], change_pts[i+1])] for i in range(len(change_pts)-1)]
     dfs = [df[(xs[i][0] <= df["time"]) & (df["time"] <= xs[i][-1])] for i in range(len(xs))]
-    return dfs
+    return dfs, xs
+
+
 if __name__ == '__main__':
     app.run_server(debug=True)
